b75dac034ab921c5e266cc0807867894ee4af9ac
cqlucasufrj <lucascq.20222@poli.ufrj.br>
2025-03-10 03:38:37 -0300

    podólogos cadastrados

--- podologos/src/app/(pages)/(SignedIn)/PodologosCadastrados/page.tsx
+++ podologos/src/app/(pages)/(SignedIn)/PodologosCadastrados/page.tsx
@@ -1,10 +1,16 @@
 'use client';
 import { CustomStyles } from '@/Components/TableStyle/index';
-import DataTable, { createTheme } from 'react-data-table-component';
+import DataTable from 'react-data-table-component';
 import Image from 'next/image';
 import PacienteImage from '@/assets/PacienteImage.svg';
+import api from '@/services/axios';
+import { useEffect, useState } from 'react';
+import ReactLoading from 'react-loading';
 
 export default function PodologosCadastrados() {
+  const [dadosPodologos, setDadosPodologos] = useState<any>();
+  const [isLoading, setIsLoading] = useState(true);
+
   const columns = [
     {
       name: 'Nome',
@@ -34,6 +40,45 @@ export default function PodologosCadastrados() {
     {
       name: 'CEP',
       selector: (row: any) => '12345-678',
+    },
+  ];
+
+  const colunasTabela = [
+    {
+      name: 'Nome',
+      selector: (row : any) => {
+        return (
+          <div className='flex items-center gap-2'>
+            <div className="w-12 h-12 relative">
+              <Image alt='' fill src={row.user.profile_picture} className="object-cover rounded-full" />
+            </div>
+            <p className='whitespace-nowrap'>{row.user.first_name + " " + row.user.last_name}</p>
+          </div>
+        )
+      },
+    },
+    {
+      name: 'Formação',
+      selector: (row : any) => {
+        return (
+          <div className='flex flex-col items-start gap-2'>
+            <p className='whitespace-nowrap'>{row.degree_type + " em Podologia"}</p>
+            <p className='whitespace-nowrap'>{row.institution + "/" + row.degree_year}</p>
+          </div>
+        )
+      },
+    },
+    {
+      name: 'Email',
+      selector: (row : any) => row.user.email,
+    },
+    {
+      name: 'Telefone',
+      selector: (row : any) => row.user.phone_number,
+    },
+    {
+      name: 'CEP',
+      selector: (row : any) => row.user.cep,
     },
   ];
 
@@ -73,17 +118,48 @@ export default function PodologosCadastrados() {
     },
   ];
 
+  const buscarPodologos = async () => {
+    console.log('buscando podologos');
+    setIsLoading(true);
+    try {
+      const response = await api.get('/doctor/medicos-autorizados');
+      setDadosPodologos(response.data);
+      console.log(response.data);
+    } catch (err: any) {
+      console.log(err);
+      console.log(err.response.data);
+      console.log(err.response.status);
+    }
+    setIsLoading(false);
+  };
+  
+  useEffect(() => {
+    if (isLoading && !dadosPodologos) {
+      buscarPodologos();
+    }
+  });
+
   return (
     <div className='flex h-full w-full flex-col gap-3 overflow-auto px-14 py-6'>
       <h1 className='text-[30px] font-bold text-azul'>Podólogos cadastrados</h1>
+      {isLoading 
+      ?
+      <ReactLoading
+        type="spin"
+        color="#2087ed"
+        height={"30px"}
+        width={"30px"}
+        className='m-auto'
+      />
+      :
       <div className='rounded-2xl shadow-lg shadow-cinza'>
         <DataTable
           responsive
-          columns={columns}
-          data={data}
+          columns={colunasTabela}
+          data={dadosPodologos}
           customStyles={CustomStyles}
         />
-      </div>
+      </div>}
     </div>
   );
 }
